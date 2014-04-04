ce87c4d4c628a5fa5fef1d41113d4376eaf3d385
Will Gester <willgester@gmail.com>
2014-04-04 15:44:11 -0700

    added header

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,14 @@
 define(function(require, exports, module) {
-    var Surface         = require('famous/core/Surface');
-    var Modifier        = require('famous/core/Modifier');
-    var Transform       = require('famous/core/Transform');
-    var View            = require('famous/core/View');
+    var Surface          = require('famous/core/Surface');
+    var Modifier         = require('famous/core/Modifier');
+    var Transform        = require('famous/core/Transform');
+    var View             = require('famous/core/View');
+    var ImageSurface     = require('famous/surfaces/ImageSurface');
+    var SequentialLayout = require('famous/views/SequentialLayout');
 
     function App() {
         View.apply(this, arguments);
-
+        _createHeader.call(this);
     }
 
     App.prototype = Object.create(View.prototype);
@@ -15,5 +17,18 @@ define(function(require, exports, module) {
     App.DEFAULT_OPTIONS = {};
 
 
+    _createHeader = function() {
+        this.header = new ImageSurface({
+            size: [undefined, 0.105 * window.innerHeight],
+            content: 'img/header1.png'
+        });
+        this.add(this.header);
+    }
+
+
     module.exports = App;
 });
+
+//lighter bacc45
+//darker  b0ba45
+//0.1911
