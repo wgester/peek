dcd37a03a8e8232b5411354b0558921ab6dd0a81
Will Gester <willgester@gmail.com>
2014-04-04 17:00:25 -0700

    added day surfaces, and number text

--- src/app/WeekView.js
+++ src/app/WeekView.js
@@ -9,6 +9,7 @@ define(function(require, exports, module) {
         View.apply(this, arguments);
         _createBackground.call(this);
         _addBorder.call(this);
+        _addDays.call(this);
     };
 
     App.prototype = Object.create(View.prototype);
@@ -34,6 +35,39 @@ define(function(require, exports, module) {
             }
         });
         this._add(line);
+    };
+
+    _addDays = function() {
+        for (var i = 0; i < 7; i++) {
+            var day = new View({
+                size: [window.innerWidth/7, 0.1911 * window.innerHeight]
+            });
+
+            var daySurface = new Surface({
+                size: [window.innerWidth/7, 0.1911 * window.innerHeight]
+            });
+            var dayModifier = new Modifier({
+                transform: Transform.translate(i * window.innerWidth/7, 0, 0)
+            });
+            
+            _addDayText.call(day, i);
+            this._add(dayModifier).add(daySurface);
+            this._add(day);
+        }
+    };
+
+    _addDayText = function(i) {
+            var date = new Surface({
+                content: '0' + i + '',
+                properties: {
+                    fontSize: '1.5em'
+                },
+                size: [0,0]
+            });
+            var dateModifier = new Modifier({
+                transform: Transform.translate(0.25 * (window.innerWidth / 7) + i * window.innerWidth / 7, 0.12 * window.innerHeight, 0)
+            })
+            this._add(dateModifier).add(date);
     };
 
     App.prototype.getSize = function getSize() {
