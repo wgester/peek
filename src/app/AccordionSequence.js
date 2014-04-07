914934499334e17eee2c9077a4926c1750cc3866
Will Gester <willgester@gmail.com>
2014-04-07 13:56:32 -0700

    added accordion layout

---
+++ src/app/AccordionSequence.js
@@ -0,0 +1,5 @@
+define(function(require, exports, module) {
+  var OptionsManager   = require('famous/core/OptionsManager');
+  var SequentialLayout = require('famous/core/SequentialLayout');
+  
+});
